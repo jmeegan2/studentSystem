8ae97ff505f68f563d0a0b8e4a5ef47a6c6f0c0c
jmeegan2 <jmeegan8@gmail.com>
2023-01-16 11:50:44 -0500

    advised me to change java version from 11 but still not connecting to mySql

--- src/main/java/com/JamesMeegan/studentSystem/model/Student.java
+++ src/main/java/com/JamesMeegan/studentSystem/model/Student.java
@@ -1,4 +1,46 @@
 package com.JamesMeegan.studentSystem.model;
 
-public class Student {
-}
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+    @Entity
+    public class Student {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int ID;
+        private String name;
+        private String address;
+
+        public Student() {
+        }
+
+        public int getID() {
+            return ID;
+        }
+
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
+
+
--- src/main/java/com/JamesMeegan/studentSystem/repository/StudentRepository.java
+++ src/main/java/com/JamesMeegan/studentSystem/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.JamesMeegan.studentSystem.repository;
 
-public class StudentRepository {
+import com.JamesMeegan.studentSystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository  extends JpaRepository<Student,Integer>{
 }
